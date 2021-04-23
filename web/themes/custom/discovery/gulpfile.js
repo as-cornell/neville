32382ecf1a4fc461104d77634bc4dda0aec36d30
bobmarteal <bobmarteal@gmail.com>
2021-04-23 12:54:18 -0400

    gulpping - globs

--- web/themes/custom/discovery/gulpfile.js
+++ web/themes/custom/discovery/gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require("gulp-autoprefixer");
 const cssmin = require("gulp-cssmin");
-const sassGlob = ('gulp-sass-glob');
+const sassGlob = require('gulp-sass-glob');
 const stripCssComments = require("gulp-strip-css-comments");
 
 const sourcemaps = require('gulp-sourcemaps');
@@ -28,18 +28,12 @@ const config = {
 function sassProcessor() {
   // 1 where is css
   return gulp.src(config.scss)
-    // init sourcemaps
-  
-    .pipe(sourcemaps.init())
-  // 2 pass that file to sass compiler
-    .pipe(sass()).on('error', sass.logError)
-  // auto prefix
-    .pipe(autoprefixer({ browsers: ["last 2 version"] }))
     
-
-    // write sourcemaps
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass()).on('error', sass.logError)
+    .pipe(autoprefixer({ browsers: ["last 2 version"] }))
     .pipe(sourcemaps.write('.'))
-  // 3 where do they go?
     .pipe(gulp.dest(config.cssDir))
 }
 
@@ -50,7 +44,7 @@ function cssProcessor() {
   .pipe(gulp.dest(config.cssMin))
 }
 
-
+// need to run cssmin on /css. setting the library up to look at /css_min breaks source maps. must be in same directory as the css being used.
 
 function watch() {
   browserSync.init({
