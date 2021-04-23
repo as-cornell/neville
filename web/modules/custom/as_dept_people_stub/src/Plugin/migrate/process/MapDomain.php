7998cb69519f1ba18f1fd6dbfe8dd8a3ef0b6772
markewilson <mark.e.wilson@gmail.com>
2021-04-23 12:10:50 -0400

    updates routes for first set of department dev domains

--- web/modules/custom/as_dept_people_stub/src/Plugin/migrate/process/MapDomain.php
+++ web/modules/custom/as_dept_people_stub/src/Plugin/migrate/process/MapDomain.php
@@ -6,6 +6,7 @@ use Drupal\migrate\MigrateException;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Map departments_programs to domain access
@@ -20,38 +21,34 @@ class MapDomain extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
     try {
 
-      if (!empty($value)) {
-        $domainstring = $value;
-        // break string into title and uri
-        $domainarray = explode(', ', $domainstring);
-        $domains = array();
-        foreach ($domainarray as $key => $domain) {
+    if (!empty($value)) {
+      // replace department name with domain key
+      //$domainstring = $value;
+      $deptname = explode(',', $value)[0];
+      //$deptname = 'History';
+      $departments = array('Molecular Biology and Genetics', 'Institute for Comparative Modernities', 'American Studies Program','Government', 'Music' );
+      $domainids   = array('mbg_as_cornell_edu', 'icmdev_as_cornell_edu', 'americanstudies_as_cornell_edu', 'government_as_cornell_edu', 'music_as_cornell_edu');
+      $domainmap = str_replace($departments, $domainids, $deptname);
 
-          if ($domain == 'Molecular Biology and Genetics') {
-              $domain = 'dept1_as_cornell_edu';
-            }
-          elseif ($domain == 'Music') {
-              $domain = 'dept2_as_cornell_edu';
-            }
-          elseif ($domain == 'Romance Studies') {
-              $domain = 'dept3_as_cornell_edu';
-            }
-          elseif ($domain == 'Government') {
-              $domain = 'dept4_as_cornell_edu';
-            }else{
-              $domain = 'dept5_as_cornell_edu';
-            }
-            $domains[$key]['domain'] = $domain;
-
-          }
-        }
+     // $termids = \Drupal::entityQuery('taxonomy_term')
+       // ->condition('vid', 'departments_programs')
+       // ->condition('name', $deptname)
+       // ->execute();
+       // if (!empty($termids)){
+         // foreach($termids as $tid)
+           // {
+             // $term = Term::load($tid);
+             // $domainid = $term->get('field_domain_access_target_id')->value;
+            //}
+          //}
       }
     }
     catch (\Exception $e) {
       throw new MigrateException('Invalid department name.');
     }
-    return $domains;
+    return $domainmap;
   }
 }
