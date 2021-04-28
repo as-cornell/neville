55fcb0777011a3c59a24d10bba551861703e2464
bobmarteal <bobmarteal@gmail.com>
2021-04-28 09:32:48 -0400

    new gulps

--- web/themes/custom/discovery/gulpfile.js
+++ web/themes/custom/discovery/gulpfile.js
@@ -4,60 +4,55 @@ const browserSync = require('browser-sync').create();
 const autoprefixer = require("gulp-autoprefixer");
 const cssmin = require("gulp-cssmin");
 const sassGlob = require('gulp-sass-glob');
-const stripCssComments = require("gulp-strip-css-comments");
+
 
 const sourcemaps = require('gulp-sourcemaps');
-
-
-// HEY Bob need to install autoprefixer and cssnano instead of the gulp versions to use with postcss
-// https://stackoverflow.com/questions/40090111/postcss-error-object-object-is-not-a-postcss-plugin
-
-// const autoprefixer = require ('autoprefixer');
-
 
 const config = {
   scss: './scss/**/*.scss',
   cssDir: './css',
-  cssFiles: './css/*.css',
-  cssMin: './css_min'
+  cssFiles: './css/*.css'
 };
 
 
 
 //compile scss into css
+// 1. init sourcemaps
+// 2. glob scss
+// 3. Sassify
+// 4. autoprefixer
+// 5. write sourcemaps to same dir as css
+// 6. minifiy css
+// 7. write to /css
+
 function sassProcessor() {
-  // 1 where is css
   return gulp.src(config.scss)
-    
     .pipe(sourcemaps.init())
     .pipe(sassGlob())
     .pipe(sass()).on('error', sass.logError)
     .pipe(autoprefixer({ browsers: ["last 2 version"] }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(config.cssDir))
-}
-
-// minify and send to /css_min
-function cssProcessor() {
-  return gulp.src(config.cssFiles)
     .pipe(cssmin())
-  .pipe(gulp.dest(config.cssMin))
+    .pipe(gulp.dest(config.cssDir))
+  .pipe(browserSync.stream());
 }
 
-// need to run cssmin on /css. setting the library up to look at /css_min breaks source maps. must be in same directory as the css being used.
 
 function watch() {
   browserSync.init({
-    server: {
-      baseDir: './'
-    }
+    proxy: 'http://appserver_nginx.neville.internal',
+    socket: {
+      domain: 'https://bs.neville.lndo.site/',
+      port: 80
+    },
+    open: false,
+    logLevel: "debug",
+    logConnections: true,
   });
   gulp.watch(config.scss, sassProcessor);
-  gulp.watch(config.cssFiles, cssProcessor);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./**/*.twig').on('change', browserSync.reload);
 }
 
 
 exports.sassProcessor = sassProcessor;
-exports.cssprocessor = cssProcessor;
 exports.watch = watch;
