3a4c852b8ee2b3eb3e00eef1c446b36ea2c45f31
bobmarteal <bobmarteal@gmail.com>
2021-05-10 09:56:35 -0400

    working on navigations

--- web/themes/custom/discovery/js/main-menu.js
+++ web/themes/custom/discovery/js/main-menu.js
@@ -1,11 +1,11 @@
 // document is ready to go
 (function ($) {
   console.log('hello');
-  $(".nav-opener").on({
+  $(".nav--main-opener").on({
     click: function () {
       $(".header__searchForm > form").removeClass("shown");
 
-      $(".mainNav").toggleClass("shown");
+      $(".nav--main").toggleClass("shown");
       
       $(".main").toggleClass("fade");
     },
@@ -17,32 +17,36 @@
   //
 
   // Add aria-haspopup true to links with popups
-  $(".mainNav__toggle").prev().attr("aria-haspopup", "true");
+  $(".nav--main-opener").prev().attr("aria-haspopup", "true");
 
   $(".subNav a").attr("tabindex", -1);
 
 
   // if a menu-button is clicked...
-  $(".mainNav__subNavToggle").click(function (e) {
+  $(".subNavToggle").click(function (e) {
+    // the button
     var _this = $(this);
+    // the LI
     var parent = $(_this.parent());
+    // the preceding <a>
     var linkContent = $(_this.prev());
+    // rotate the button
     $(this).toggleClass("rotated");
-
+    // the child list
     var next = $(_this.next());
 
     console.log(linkContent);
 
-    if (!next.hasClass("subNav--expanded")) {
-      $(next).addClass("subNav--expanded");
-      $(parent).addClass("withSubNav--expanded");
+    if (!next.hasClass("expanded")) {
+      $(next).addClass("expanded");
+      $(parent).addClass("expanded");
      
       _this.next().find("a").removeAttr("tabindex", -1);
 
       // remove sub-expaneded
     } else {
-      $(next).removeClass("subNav--expanded");
-      $(parent).removeClass("withSubNav--expanded");
+      $(next).removeClass("expanded");
+      $(parent).removeClass("expanded");
       _this.next().find("a").attr("tabindex", -1);
     }
 
