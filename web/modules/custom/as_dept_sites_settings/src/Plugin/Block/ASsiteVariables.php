3cf662485d9b86097845a8e3bae01e1d87318801
markewilson <mark.e.wilson@gmail.com>
2021-05-20 11:40:28 -0400

    more work on ASsiteVariables.php and adds block--site-variables-block.html.twig

--- web/modules/custom/as_dept_sites_settings/src/Plugin/Block/ASsiteVariables.php
+++ web/modules/custom/as_dept_sites_settings/src/Plugin/Block/ASsiteVariables.php
@@ -10,7 +10,7 @@ use Drupal\Core\Block\BlockPluginInterface;
  *
  * @Block(
  *   id = "site_variables_block",
- *   admin_label = @Translation("De[partment Site Settings Block"),
+ *   admin_label = @Translation("Department Site Settings Block"),
  *   category = @Translation("Departments"),
  * )
  */
@@ -26,30 +26,9 @@ class ASsiteVariables extends BlockBase implements BlockPluginInterface {
 
     $build = [];
     $markup = '';
-    $config = $this->getConfiguration();
-    dump($config);
-    if (!empty($config['netid'])) {
-      $netid = $config['netid'] ;
-    }
+    //$config = $this->getConfiguration();
+    $build['events_block']['#markup'] = $markup;
 
-    $info = as_people_ldap_get_netid_ldap($netid);
-
-    if (!empty($info[0])) {
-      if (!empty($info[0]['cornelleducampusaddress'][0])) {
-        $markup = $info[0]['cornelleducampusaddress'][0] .'<br />';
-          }
-      if (!empty($info[0]['mail'][0])) {
-        $markup = $markup. '<a href="mailto:'.$info[0]['mail'][0].'">'.$info[0]['mail'][0].'</a><br />';
-          }
-      if (!empty($info[0]['cornelleducampusphone'][0])) {
-        $markup = $markup. $info[0]['cornelleducampusphone'][0].'<br />';
-          }
-        $build['events_block']['#markup'] = $markup;
-   // } else { // There were no events
-     // $build['events_block']['#markup'] = '<main>
-                //<p>No LDAP data for '.$netid.'</p>
-                //</main>';
-    }
 
     return $build;
   }
