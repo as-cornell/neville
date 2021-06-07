1c341ffec8da5870ac2b3b498881d0c8ed81fc72
bobmarteal <bobmarteal@gmail.com>
2021-06-07 16:54:54 -0400

    adds on this page nav js

---
+++ web/themes/custom/discovery/js/on-this-page-nav.js
@@ -0,0 +1,65 @@
+/*
+  On this page "OTP" navigation
+  - add .active to first item
+  - observers to change active to different links based on location of intersection sections
+*/
+
+(function ($) {
+  console.log('otpnavbaby');
+  $(".otpNav").first().first().addClass("active");
+  $(".otpToggle").on({
+    click: function () {
+      $(".otpNav").toggleClass("expanded");
+    },
+  });
+})(jQuery);
+
+const targets = document.querySelectorAll(".pageSection");
+const navOptions = {
+  threshold: 0,
+  rootMargin: "-200px",
+};
+
+const navObserver = new IntersectionObserver((entries, navObserver) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      document.querySelector(".active").classList.remove("active");
+      var id = entry.target.getAttribute("id");
+      console.log(id);
+      var newLink = document
+        .querySelector(`[href="#${id}"]`)
+        .classList.add("active");
+    }
+  });
+}, navOptions);
+
+targets.forEach((target) => {
+  navObserver.observe(target);
+});
+
+
+
+// watch .title if not intersecting add class 'stuck' to otpNav
+const pageTitle = document.querySelector(".main > .title");
+const mainContainer = document.querySelector(".main");
+
+const otpNavOptions = {
+  threshold: 0,
+  // rootMargin: "200px 0px 0px 0px",
+};
+
+const otpNavObserver = new IntersectionObserver(function (entries, optNavObserver) { 
+  entries.forEach(entry => {
+    // console.log(entry.target);
+    if (!entry.isIntersecting) {
+      mainContainer.classList.add('otpStuck');
+    } else {
+      mainContainer.classList.remove('otpStuck');
+    }
+  });
+}, otpNavOptions);
+
+otpNavObserver.observe(pageTitle);
