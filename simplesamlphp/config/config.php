6ed397246b1a41f74291403d5cd17f7d803a3980
markewilson <mark.e.wilson@gmail.com>
2021-06-22 15:09:42 -0400

    adds database integration for saml and updates web location in .platform.app.yaml

--- simplesamlphp/config/config.php
+++ simplesamlphp/config/config.php
@@ -1059,6 +1059,24 @@ $config = array(
      */
     'store.type'                    => 'phpsession',
 
+    /*
+     * The DSN the sql datastore should connect to.
+     *
+     * See http://www.php.net/manual/en/pdo.drivers.php for the various
+     * syntaxes.
+     */
+    'store.sql.dsn'                 => 'sqlite:/path/to/sqlitedatabase.sq3',
+
+    /*
+     * The username and password to use when connecting to the database.
+     */
+    'store.sql.username' => null,
+    'store.sql.password' => null,
+
+    /*
+     * The prefix we should use on our tables.
+     */
+    'store.sql.prefix' => 'SimpleSAMLphp',
 
     /*
      * The hostname and port of the Redis datastore instance.
@@ -1083,6 +1101,29 @@ $config['logging.handler'] = 'errorlog';
 $config['metadata.sources'] = [
    ['type' => 'flatfile', 'directory' =>  dirname(__DIR__) . '/metadata'],
 ];
+
+// Setup the database connection for all parts of SimpleSAML.
+if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
+  $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), TRUE);
+  foreach ($relationships['saml'] as $instance) {
+    if (!empty($instance['query']['is_master'])) {
+      $dsn = sprintf("%s:host=%s;dbname=%s",
+        $instance['scheme'],
+        $instance['host'],
+        $instance['path']
+      );
+      $config['database.dsn'] = $dsn;
+      $config['database.username'] = $instance['username'];
+      $config['database.password'] = $instance['password'];
+      $config['store.type'] = 'sql';
+      $config['store.sql.dsn'] = $dsn;
+      $config['store.sql.username'] = $instance['username'];
+      $config['store.sql.password'] = $instance['password'];
+      $config['store.sql.prefix'] = 'simplesaml';
+
+    }
+  }
+}
 
 // Set the salt value from the Platform.sh entropy value, provided for this purpose.
 if (isset($_ENV['PLATFORM_PROJECT_ENTROPY'])) {
