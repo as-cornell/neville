4e59defa832c4c13e4c76867e6338fbad82546c3
markewilson <mark.e.wilson@gmail.com>
2021-07-27 16:35:51 -0400

    adds cleantitle function to convery html entities in article titles

---
+++ web/modules/custom/as_dept_articles_migrate/src/Plugin/migrate/process/CleanTitle.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\as_dept_articles_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+
+/**
+ * Clean article title to remove special characters
+ *
+ * @MigrateProcessPlugin(
+ *   id = "cleantitle",
+ * )
+ */
+class CleanTitle extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    try {
+
+    if (!empty($value)) {
+      // decode special characters
+      $cleantitle = htmlspecialchars_decode($value);
+      $cleantitle = html_entity_decode($cleantitle, ENT_QUOTES | ENT_XML1, 'UTF-8');
+      //dump($cleantitle);
+      }
+    }
+    catch (\Exception $e) {
+      throw new MigrateException('Invalid department name.');
+    }
+    //dump($cleantitle);
+    return $cleantitle;
+  }
+}
