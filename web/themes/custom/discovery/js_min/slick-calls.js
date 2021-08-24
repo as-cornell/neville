91d647641200952c1c0e735c6827a4238317c151
bobmarteal <bobmarteal@gmail.com>
2021-08-24 13:29:55 -0400

    teaser flickity carousel

---
+++ web/themes/custom/discovery/js_min/slick-calls.js
@@ -0,0 +1,1 @@
+jQuery(".carousel").slick({dots:!0,infinite:!0,speed:300,slidesToShow:4,slidesToScroll:4,responsive:[{breakpoint:1224,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0,dots:!0}},{breakpoint:782,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]});
