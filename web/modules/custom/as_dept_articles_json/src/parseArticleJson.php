93de0c288aa2bfbfef7bcee5d67af2e3787194f0
markewilson <mark.e.wilson@gmail.com>
2021-09-03 08:47:50 -0400

    gives article_thumnail the business

--- web/modules/custom/as_dept_articles_json/src/parseArticleJson.php
+++ web/modules/custom/as_dept_articles_json/src/parseArticleJson.php
@@ -64,7 +64,7 @@ class parseArticleJson extends \Twig_Extension
           }else{
             $article_record['imagepath'] = 'https://as.cornell.edu/sites/default/files/styles/4_5/public/field/image/Klarmanarticle.jpg';
             $article_record['thumbnail_imagepath'] = 'https://as.cornell.edu/sites/default/files/styles/1_1_thumbnail_forced/public/field/image/Klarmanarticle.jpg';
-            }
+              }
             }
           }
       if (!empty($article_json['included'][3])) {
@@ -74,6 +74,16 @@ class parseArticleJson extends \Twig_Extension
 
           }else{
             $article_record['newsletter_imagepath'] = 'https://as.cornell.edu/sites/default/files/styles/6_4_newsletter/public/field/image/Klarmanarticle.jpg';
+            }
+          }
+        }
+      if (!empty($article_json['included'][5])) {
+        if ($article_json['included'][5]['type'] == 'file--file') {
+          if (!empty($article_json['included'][5]['attributes']['filename'])) {
+            $article_record['thumbnail_imagepath'] = 'https://as.cornell.edu/sites/default/files/styles/1_1_thumbnail_forced/public/field/image/'.$article_json['included'][5]['attributes']['filename'];
+
+          }else{
+            $article_record['thumbnail_imagepath'] = 'https://as.cornell.edu/sites/default/files/styles/1_1_thumbnail_forced/public/field/image/Klarmanarticle.jpg';
             }
           }
         }
