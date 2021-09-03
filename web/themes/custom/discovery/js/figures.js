55ce2a9e0c904aa8a98305167075b061721366aa
bobmarteal <bobmarteal@gmail.com>
2021-09-03 15:45:36 -0400

    adds fancy image caption

--- web/themes/custom/discovery/js/figures.js
+++ web/themes/custom/discovery/js/figures.js
@@ -1,11 +1,12 @@
 
 (function ($) {
   console.log('it figures');
-  $(".figure--hasCaption").addClass("active").append("<button class='toggle--figcaption'><svg class='icon' viewbox='0 0 52 32'><use xlink:href='#burger'></use></svg></button>");
+  $(".article__thumbnail > .figure--hasCaption, .pcWrapper--ftb .figure--hasCaption").addClass("active").append("<button class='toggle--figcaption'><svg class='icon' viewbox='0 0 68 80'><use xlink:href='#figure-caption'></use></svg></button>");
 
   $(".toggle--figcaption").on({
     click: function () {
       var toggle = $(this);
+      $(toggle).attr("")
       $(toggle).prev().toggleClass("shown");
       $(toggle).parent().toggleClass("showCaption");
     }
@@ -16,6 +17,3 @@
   // when button clicked toggle class "shown" on figcaption
 })(jQuery);
 
-{/* <svg class='icon icon--search' viewbox='0 0 52 32'> */}
-					// <use xlink:href='#search'></use>
-				// </svg>
